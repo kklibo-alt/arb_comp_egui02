b937213c2fe47161033d7971fba650328af2d47b
kklibo-alt <kklibo4+alt@gmail.com>
2025-06-06 16:25:43 -0700

    ui: add document map

--- egui_hex07/src/hex_app.rs
+++ egui_hex07/src/hex_app.rs
@@ -1,6 +1,9 @@
 use crate::diff::{self, HexCell};
 use arb_comp06::{bpe::Bpe, matcher, re_pair::RePair, test_patterns, test_utils};
-use egui::{Color32, Context, RichText, Ui};
+use egui::{
+    Color32, ColorImage, Context, Pos2, Rect, Response, RichText, Sense, Stroke, StrokeKind,
+    TextureHandle, TextureOptions, Ui,
+};
 use egui_extras::{Column, TableBody, TableBuilder, TableRow};
 use rand::Rng;
 use std::sync::{
@@ -28,6 +31,84 @@ enum DiffMethod {
     RePairGreedy00,
 }
 
+#[derive(Debug, Default, Copy, Clone)]
+/// Represents an active mousedrag on a scrolling object in the UI.
+struct ScrollDrag {
+    /// The mouse position when the drag started.
+    start_pos: Pos2,
+    /// The scroll value when the drag started.
+    start_scroll: Ratio,
+}
+
+#[derive(Debug, Default, Copy, Clone)]
+/// This value is a unitless ratio of something else.
+struct Ratio(f32);
+
+impl Ratio {
+    fn valid_or_zero(ratio: f32) -> Self {
+        Self(if ratio.is_finite() { ratio } else { 0.0 })
+    }
+}
+
+#[derive(Debug, Default)]
+/// Models a rectangular document area with a vertically-sliding view window.
+struct DocumentViewState {
+    /// The distance from the top of the document to the top of the
+    /// view window, as a proportion of document height.
+    scroll_from_top: Ratio,
+    /// The height of the view window, as a proportion of document height.
+    window_height: Ratio,
+}
+
+impl DocumentViewState {
+    pub fn ratio_from_height(height: f32, document_rect: Rect) -> Ratio {
+        Ratio::valid_or_zero(height / document_rect.height())
+    }
+
+    pub fn ratio_from_pos(pos: Pos2, document_rect: Rect) -> Ratio {
+        Self::ratio_from_height(pos.y - document_rect.top(), document_rect)
+    }
+
+    pub fn scroll_from_top(&self) -> Ratio {
+        self.scroll_from_top
+    }
+
+    #[allow(dead_code)]
+    pub fn window_height(&self) -> Ratio {
+        self.window_height
+    }
+
+    /// The view window on a full document represented by `document_rect`.
+    pub fn view_window(&self, document_rect: Rect) -> Rect {
+        let top = document_rect.top() + document_rect.height() * self.scroll_from_top.0;
+        let bottom = top + document_rect.height() * self.window_height.0;
+
+        Rect::from_min_max(
+            Pos2::new(document_rect.left(), top),
+            Pos2::new(document_rect.right(), bottom),
+        )
+    }
+
+    pub fn set_view_window_scroll(&mut self, scroll_from_top: Ratio) {
+        self.set_view_window(scroll_from_top, self.window_height);
+    }
+
+    pub fn set_view_window(&mut self, scroll_from_top: Ratio, window_height: Ratio) {
+        self.scroll_from_top = scroll_from_top;
+        self.window_height = window_height;
+
+        // Eventually: decide how to handle view windows that exceed document bounds.
+    }
+
+    pub fn is_in_view_window(&self, document_rect: Rect, pos: Pos2) -> bool {
+        self.view_window(document_rect).contains(pos)
+    }
+
+    pub fn center_view_window(&mut self, center_on: Ratio) {
+        self.scroll_from_top = Ratio(center_on.0 - 0.5 * self.window_height.0)
+    }
+}
+
 pub struct HexApp {
     source_name0: Option<String>,
     source_name1: Option<String>,
@@ -35,12 +116,19 @@ pub struct HexApp {
     pattern1: Arc<Mutex<Option<Vec<u8>>>>,
     diffs0: Arc<Mutex<Vec<HexCell>>>,
     diffs1: Arc<Mutex<Vec<HexCell>>>,
+    diffs_texture0: TextureHandle,
+    diffs_texture1: TextureHandle,
     file_drop_target: WhichFile,
     diff_method: DiffMethod,
     update_new_id_rx: Option<mpsc::Receiver<usize>>,
     egui_context: Context,
     job_running: Arc<AtomicBool>,
     cancel_job: Arc<AtomicBool>,
+    document_view_state: DocumentViewState,
+    table_height: f32,
+    document_map_drag: Option<ScrollDrag>,
+    document_map_boolean_diff: bool,
+    hex_grid_width: usize,
 }
 
 fn random_pattern() -> Vec<u8> {
@@ -50,6 +138,13 @@ fn random_pattern() -> Vec<u8> {
 
 impl HexApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        let texture_handle0 =
+            cc.egui_ctx
+                .load_texture("document_map0", ColorImage::default(), Default::default());
+        let texture_handle1 =
+            cc.egui_ctx
+                .load_texture("document_map1", ColorImage::default(), Default::default());
+
         let mut result = Self {
             source_name0: Some("zeroes0".to_string()),
             source_name1: Some("zeroes1".to_string()),
@@ -57,12 +152,19 @@ impl HexApp {
             pattern1: Arc::new(Mutex::new(Some(vec![0; 1000]))),
             diffs0: Arc::new(Mutex::new(vec![])),
             diffs1: Arc::new(Mutex::new(vec![])),
+            diffs_texture0: texture_handle0,
+            diffs_texture1: texture_handle1,
             file_drop_target: WhichFile::File0,
             diff_method: DiffMethod::ByIndex,
             update_new_id_rx: None,
             egui_context: cc.egui_ctx.clone(),
             job_running: Arc::new(AtomicBool::new(false)),
             cancel_job: Arc::new(AtomicBool::new(false)),
+            document_view_state: DocumentViewState::default(),
+            table_height: 0.0,
+            document_map_drag: None,
+            document_map_boolean_diff: true,
+            hex_grid_width: 16,
         };
 
         result.update_diffs();
@@ -100,7 +202,13 @@ impl HexApp {
         let diffs0 = self.diffs0.clone();
         let diffs1 = self.diffs1.clone();
 
+        let mut diffs_texture0 = self.diffs_texture0.clone();
+        let mut diffs_texture1 = self.diffs_texture1.clone();
+
         let diff_method = self.diff_method;
+        let hex_grid_width = self.hex_grid_width;
+        let document_map_boolean_diff = self.document_map_boolean_diff;
+
         #[cfg(not(target_arch = "wasm32"))]
         let egui_context = self.egui_context.clone();
 
@@ -173,7 +281,7 @@ impl HexApp {
                                 tx.send(x).unwrap();
                                 request_repaint();
                             };
-                            
+
                             let mut re_pair = RePair::new_iterative(&[pattern0, pattern1]);
                             while re_pair.init_in_progress.is_some() {
                                 re_pair.init_step(Some(f));
@@ -193,6 +301,62 @@ impl HexApp {
                 } else {
                     (vec![], vec![])
                 };
+
+            let columns = hex_grid_width;
+            let rows = std::cmp::max(new_diffs0.len(), new_diffs1.len()).div_ceil(columns);
+
+            let cells_to_image = |cells: &[HexCell]| -> ColorImage {
+                let mut color_image = ColorImage::new([columns, rows], Color32::TRANSPARENT);
+
+                for (&h, p) in cells.iter().zip(color_image.pixels.iter_mut()) {
+                    match h {
+                        HexCell::Same {
+                            value: _,
+                            source_id,
+                        } => {
+                            *p = if document_map_boolean_diff {
+                                Color32::DARK_GREEN
+                            } else {
+                                HexApp::contrast(HexApp::color(source_id))
+                            };
+                        }
+                        HexCell::Diff {
+                            value: _,
+                            source_id,
+                        } => {
+                            *p = if document_map_boolean_diff {
+                                Color32::LIGHT_GREEN
+                            } else {
+                                HexApp::color(source_id)
+                            };
+                        }
+                        HexCell::Blank => {
+                            *p = Color32::from_rgba_premultiplied(0, 0, 0, 128);
+                        }
+                    }
+                }
+                color_image
+            };
+
+            let color_image0 = cells_to_image(&new_diffs0);
+            let color_image1 = cells_to_image(&new_diffs1);
+
+            // Can this block the main thread?
+            diffs_texture0.set(
+                color_image0,
+                TextureOptions {
+                    magnification: egui::TextureFilter::Nearest,
+                    ..Default::default()
+                },
+            );
+            diffs_texture1.set(
+                color_image1,
+                TextureOptions {
+                    magnification: egui::TextureFilter::Nearest,
+                    ..Default::default()
+                },
+            );
+
             log::info!("started updating diffs");
             {
                 let mut diffs0 = diffs0.lock().unwrap();
@@ -255,28 +419,28 @@ impl HexApp {
         });
     }
 
-    fn add_body_contents(&self, body: TableBody<'_>) {
-        fn color(c: usize) -> Color32 {
-            let hi: u8 = 255;
-            let lo: u8 = 128;
-            match c % 6 {
-                0 => Color32::from_rgb(hi, lo, lo),
-                1 => Color32::from_rgb(hi, hi, lo),
-                2 => Color32::from_rgb(lo, hi, lo),
-                3 => Color32::from_rgb(lo, hi, hi),
-                4 => Color32::from_rgb(lo, lo, hi),
-                5 => Color32::from_rgb(hi, lo, hi),
-                _ => unreachable!(),
-            }
+    fn color(c: usize) -> Color32 {
+        let hi: u8 = 255;
+        let lo: u8 = 128;
+        match c % 6 {
+            0 => Color32::from_rgb(hi, lo, lo),
+            1 => Color32::from_rgb(hi, hi, lo),
+            2 => Color32::from_rgb(lo, hi, lo),
+            3 => Color32::from_rgb(lo, hi, hi),
+            4 => Color32::from_rgb(lo, lo, hi),
+            5 => Color32::from_rgb(hi, lo, hi),
+            _ => unreachable!(),
         }
-        fn contrast(color: Color32) -> Color32 {
-            Color32::from_rgb(
-                u8::wrapping_add(color.r(), 128),
-                u8::wrapping_add(color.g(), 128),
-                u8::wrapping_add(color.b(), 128),
-            )
-        }
+    }
+    fn contrast(color: Color32) -> Color32 {
+        Color32::from_rgb(
+            u8::wrapping_add(color.r(), 128),
+            u8::wrapping_add(color.g(), 128),
+            u8::wrapping_add(color.b(), 128),
+        )
+    }
 
+    fn add_body_contents(&self, body: TableBody<'_>) {
         let diffs0 = if let Ok(diffs0) = self.diffs0.try_lock() {
             diffs0
         } else {
@@ -289,10 +453,10 @@ impl HexApp {
             return;
         };
 
-        let hex_grid_width = 16;
+        let hex_grid_width = self.hex_grid_width;
 
         let row_height = 18.0;
-        let num_rows = 1 + std::cmp::max(diffs0.len(), diffs1.len()) / hex_grid_width;
+        let num_rows = std::cmp::max(diffs0.len(), diffs1.len()).div_ceil(hex_grid_width);
 
         body.rows(row_height, num_rows, |mut row| {
             let row_index = row.index();
@@ -304,12 +468,12 @@ impl HexApp {
                     match cell {
                         Some(&HexCell::Same { value, source_id }) => ui.label(
                             RichText::new(format!("{value:02X}"))
-                                .color(color(source_id))
+                                .color(Self::color(source_id))
                                 .monospace(),
                         ),
                         Some(&HexCell::Diff { value, source_id }) => {
-                            let color = color(source_id);
-                            let contrast = contrast(color);
+                            let color = Self::color(source_id);
+                            let contrast = Self::contrast(color);
                             ui.label(
                                 RichText::new(format!("{value:02X}"))
                                     .color(contrast)
@@ -328,18 +492,27 @@ impl HexApp {
                 (0..hex_grid_width).for_each(|i| {
                     let cell = diffs.get(i + row_index * hex_grid_width);
 
+                    fn clean_ascii(value: u8) -> char {
+                        let ch = value as char;
+                        if ch.is_ascii_control() || ch.is_ascii_whitespace() {
+                            ' '
+                        } else {
+                            ch
+                        }
+                    }
+
                     match cell {
                         Some(&HexCell::Same { value, source_id }) => ui.label(
-                            RichText::new(format!("{}", value as char))
-                                .color(color(source_id))
+                            RichText::new(format!("{}", clean_ascii(value)))
+                                .color(Self::color(source_id))
                                 .monospace(),
                         ),
                         Some(&HexCell::Diff { value, source_id }) => {
-                            let color = color(source_id);
-                            let contrast = contrast(color);
+                            let color = Self::color(source_id);
+                            let contrast = Self::contrast(color);
 
                             ui.label(
-                                RichText::new(format!("{}", value as char))
+                                RichText::new(format!("{}", clean_ascii(value)))
                                     .color(contrast)
                                     .background_color(color)
                                     .monospace(),
@@ -411,6 +584,18 @@ impl eframe::App for HexApp {
             }
         });
 
+        egui::SidePanel::right("document_map_panel")
+            .exact_width(250.0)
+            .show(ctx, |ui| {
+                draw_document_map(
+                    ui,
+                    self.diffs_texture0.clone(),
+                    self.diffs_texture1.clone(),
+                    &mut self.document_view_state,
+                    &mut self.document_map_drag,
+                );
+            });
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 ui.heading("hex diff test (egui UI)");
@@ -481,18 +666,108 @@ impl eframe::App for HexApp {
 
                 //display the new id
                 ui.label(RichText::new(format!("new id: {new_id:?}")));
+
+                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+                    ui.checkbox(&mut self.document_map_boolean_diff, "Show Diff");
+                });
             });
 
-            TableBuilder::new(ui)
-                .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                .striped(true)
-                .column(Column::auto().resizable(true))
-                .column(Column::auto().resizable(true))
-                .column(Column::auto().resizable(true))
-                .column(Column::auto().resizable(true))
-                .column(Column::remainder())
-                .header(20.0, |header| self.add_header_row(header))
-                .body(|body| self.add_body_contents(body));
+            egui::ScrollArea::horizontal().show(ui, |ui| {
+                let scroll_area_output = TableBuilder::new(ui)
+                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                    .striped(true)
+                    .column(Column::auto().resizable(true))
+                    .column(Column::auto().resizable(true))
+                    .column(Column::auto().resizable(true))
+                    .column(Column::auto().resizable(true))
+                    .column(Column::remainder())
+                    .vertical_scroll_offset(
+                        self.table_height * self.document_view_state.scroll_from_top().0,
+                    )
+                    .header(20.0, |header| self.add_header_row(header))
+                    .body(|body| self.add_body_contents(body));
+
+                let table_height = scroll_area_output.content_size.y;
+                self.table_height = table_height;
+
+                let scroll_from_top =
+                    Ratio::valid_or_zero(scroll_area_output.state.offset.y / table_height);
+                let view_window_height =
+                    Ratio::valid_or_zero(scroll_area_output.inner_rect.height() / table_height);
+
+                self.document_view_state
+                    .set_view_window(scroll_from_top, view_window_height);
+            });
         });
     }
+}
+
+fn draw_document_map(
+    ui: &mut Ui,
+    texture_h0: TextureHandle,
+    texture_h1: TextureHandle,
+    document_view_state: &mut DocumentViewState,
+    view_window_drag: &mut Option<ScrollDrag>,
+) -> Response {
+    let draw_rect = ui.max_rect();
+
+    let (response, painter) = ui.allocate_painter(draw_rect.size(), Sense::click_and_drag());
+
+    if response.clicked() {
+        if let Some(pos) = response.interact_pointer_pos() {
+            if !document_view_state.is_in_view_window(draw_rect, pos) {
+                let center = DocumentViewState::ratio_from_pos(pos, draw_rect);
+                document_view_state.center_view_window(center);
+            }
+        }
+    }
+
+    if response.drag_started() {
+        if let Some(pos) = response.interact_pointer_pos() {
+            if document_view_state.is_in_view_window(draw_rect, pos) {
+                *view_window_drag = Some(ScrollDrag {
+                    start_pos: pos,
+                    start_scroll: document_view_state.scroll_from_top(),
+                });
+            }
+        }
+    }
+    if response.dragged() {
+        if let Some(pos) = response.interact_pointer_pos() {
+            if let Some(ScrollDrag {
+                start_pos,
+                start_scroll,
+            }) = view_window_drag
+            {
+                let drag_scroll =
+                    DocumentViewState::ratio_from_height(pos.y - start_pos.y, draw_rect);
+
+                document_view_state.set_view_window_scroll(Ratio(drag_scroll.0 + start_scroll.0));
+            }
+        }
+    }
+    if response.drag_stopped() {
+        *view_window_drag = None;
+    }
+
+    painter.debug_rect(draw_rect, Color32::RED, "document_map");
+
+    painter.rect_stroke(
+        draw_rect,
+        10.0,
+        Stroke::new(1.0, Color32::ORANGE),
+        StrokeKind::Inside,
+    );
+
+    let (mut left, mut right) = draw_rect.split_left_right_at_fraction(0.5);
+    *left.right_mut() -= 1.0;
+    *right.left_mut() += 1.0;
+
+    egui::Image::new(&texture_h0).paint_at(ui, left);
+    egui::Image::new(&texture_h1).paint_at(ui, right);
+
+    let bar_rect = document_view_state.view_window(draw_rect);
+    painter.rect_filled(bar_rect, 10.0, Color32::from_white_alpha(32));
+
+    response
 }
