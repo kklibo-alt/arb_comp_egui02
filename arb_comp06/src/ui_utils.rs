056cf5aa877cbcb1f77b416efe6a045c86e6e960
kklibo-alt <kklibo4+alt@gmail.com>
2025-07-15 14:51:31 -0700

    copy matches_to_cells to ui_utils.rs
    (cherry picked from commit 032ea0f71d12bbc8abfffdcc43ed218b620a3bc7)

---
+++ arb_comp06/src/ui_utils.rs
@@ -0,0 +1,40 @@
+pub fn matches_to_cells(
+    matches: &[Matched],
+    decode: impl Fn(&Vec<TokenId>) -> Vec<u8>,
+) -> (Vec<HexCell>, Vec<HexCell>) {
+    let mut cells0 = vec![];
+    let mut cells1 = vec![];
+
+    matches.iter().for_each(|matched| match matched {
+        Matched::Same(ids) => {
+            for &id in ids {
+                cells0.append(&mut hex_cells(false, id, &decode));
+                cells1.append(&mut hex_cells(false, id, &decode));
+            }
+        }
+        Matched::Diff(ids0, ids1) => {
+            let mut block_cells0 = vec![];
+            let mut block_cells1 = vec![];
+
+            for &id in ids0 {
+                block_cells0.append(&mut hex_cells(true, id, &decode));
+            }
+            for &id in ids1 {
+                block_cells1.append(&mut hex_cells(true, id, &decode));
+            }
+
+            while block_cells0.len() < block_cells1.len() {
+                block_cells0.push(HexCell::Blank);
+            }
+
+            while block_cells1.len() < block_cells0.len() {
+                block_cells1.push(HexCell::Blank);
+            }
+
+            cells0.append(&mut block_cells0);
+            cells1.append(&mut block_cells1);
+        }
+    });
+
+    (cells0, cells1)
+}
