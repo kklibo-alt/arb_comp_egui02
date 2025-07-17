d2b24cb8f32859a7ed3cde0f01e41f1e65b6e93f
kklibo-alt <kklibo4+alt@gmail.com>
2025-07-16 21:19:59 -0700

    impl Matched::Same

--- arb_comp06/src/ui_utils.rs
+++ arb_comp06/src/ui_utils.rs
@@ -17,46 +17,70 @@ impl AlignedCells {
     pub fn new(matches: Vec<Matched>, decode: impl Fn(&Vec<TokenId>) -> Vec<u8>) -> Self {
         let mut cells0 = vec![];
         let mut cells1 = vec![];
+        let mut cells0_index_to_matches_index = RangeMap::new();
+        let mut cells1_index_to_matches_index = RangeMap::new();
+        let mut matches_index_to_cells0_alignment_offset = vec![];
+        let mut matches_index_to_cells1_alignment_offset = vec![];
 
-        matches.iter().for_each(|matched| match matched {
-            Matched::Same(ids) => {
-                for &id in ids {
-                    cells0.append(&mut hex_cells(false, id, &decode));
-                    cells1.append(&mut hex_cells(false, id, &decode));
-                }
-            }
-            Matched::Diff(ids0, ids1) => {
-                let mut block_cells0 = vec![];
-                let mut block_cells1 = vec![];
+        let mut index0 = 0;
+        let mut index1 = 0;
+        let mut offset0 = 0;
+        let mut offset1 = 0;
 
-                for &id in ids0 {
-                    block_cells0.append(&mut hex_cells(true, id, &decode));
-                }
-                for &id in ids1 {
-                    block_cells1.append(&mut hex_cells(true, id, &decode));
-                }
+        matches
+            .iter()
+            .enumerate()
+            .for_each(|(match_index, matched)| match matched {
+                Matched::Same(ids) => {
+                    let mut block_len = 0;
+                    for &id in ids {
+                        let mut new_cells = hex_cells(false, id, &decode);
+                        block_len += new_cells.len();
+                        cells0.append(&mut new_cells.clone());
+                        cells1.append(&mut new_cells);
+                    }
 
-                while block_cells0.len() < block_cells1.len() {
-                    block_cells0.push(HexCell::Blank);
-                }
+                    cells0_index_to_matches_index.insert(index0..index0 + block_len, match_index);
+                    cells1_index_to_matches_index.insert(index1..index1 + block_len, match_index);
 
-                while block_cells1.len() < block_cells0.len() {
-                    block_cells1.push(HexCell::Blank);
-                }
+                    matches_index_to_cells0_alignment_offset.push(offset0);
+                    matches_index_to_cells1_alignment_offset.push(offset1);
 
-                cells0.append(&mut block_cells0);
-                cells1.append(&mut block_cells1);
-            }
-        });
+                    index0 += block_len;
+                    index1 += block_len;
+                }
+                Matched::Diff(ids0, ids1) => {
+                    let mut block_cells0 = vec![];
+                    let mut block_cells1 = vec![];
+
+                    for &id in ids0 {
+                        block_cells0.append(&mut hex_cells(true, id, &decode));
+                    }
+                    for &id in ids1 {
+                        block_cells1.append(&mut hex_cells(true, id, &decode));
+                    }
+
+                    while block_cells0.len() < block_cells1.len() {
+                        block_cells0.push(HexCell::Blank);
+                    }
+
+                    while block_cells1.len() < block_cells0.len() {
+                        block_cells1.push(HexCell::Blank);
+                    }
+
+                    cells0.append(&mut block_cells0);
+                    cells1.append(&mut block_cells1);
+                }
+            });
 
         Self {
             matches,
             cells0,
             cells1,
-            cells0_index_to_matches_index: RangeMap::default(),
-            cells1_index_to_matches_index: RangeMap::default(),
-            matches_index_to_cells0_alignment_offset: vec![],
-            matches_index_to_cells1_alignment_offset: vec![],
+            cells0_index_to_matches_index,
+            cells1_index_to_matches_index,
+            matches_index_to_cells0_alignment_offset,
+            matches_index_to_cells1_alignment_offset,
         }
     }
 }
