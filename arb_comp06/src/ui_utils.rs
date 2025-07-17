74d1de5145af6eea50e9251e954661c9f0633618
kklibo-alt <kklibo4+alt@gmail.com>
2025-07-17 11:51:06 -0700

    CellAlignment struct

--- arb_comp06/src/ui_utils.rs
+++ arb_comp06/src/ui_utils.rs
@@ -2,12 +2,39 @@ use crate::matcher::Matched;
 use crate::test_utils::{hex_cells, HexCell};
 use crate::token::TokenId;
 
+#[derive(Debug, Default)]
+pub struct CellAlignment {
+    cells0_addresses: Vec<usize>,
+    cells1_addresses: Vec<usize>,
+    aligned_addresses: Vec<usize>,
+}
+
+impl CellAlignment {
+    fn add(&mut self, cells0_address: usize, cells1_address: usize) {
+        let prev_cells0_address = *self.cells0_addresses.last().unwrap_or(&0);
+        let prev_cells1_address = *self.cells1_addresses.last().unwrap_or(&0);
+        let prev_aligned_address = *self.aligned_addresses.last().unwrap_or(&0);
+
+        let cells0_delta = cells0_address.checked_sub(prev_cells0_address).unwrap();
+        let cells1_delta = cells1_address.checked_sub(prev_cells1_address).unwrap();
+        assert!(cells0_delta > 0);
+        assert!(cells1_delta > 0);
+
+        let new_aligned_address = prev_aligned_address + std::cmp::max(cells0_delta, cells1_delta);
+
+        self.cells0_addresses.push(cells0_address);
+        self.cells1_addresses.push(cells1_address);
+        self.aligned_addresses.push(new_aligned_address);
+    }
+}
+
 pub fn matches_to_cells(
     matches: &[Matched],
     decode: impl Fn(&Vec<TokenId>) -> Vec<u8>,
 ) -> (Vec<HexCell>, Vec<HexCell>) {
     let mut cells0 = vec![];
     let mut cells1 = vec![];
+    let mut alignment = CellAlignment::default();
 
     matches.iter().for_each(|matched| match matched {
         Matched::Same(ids) => {
